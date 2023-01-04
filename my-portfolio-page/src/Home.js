cc27ca8ff2248e37b2ad9e3b3de7ffe7146c58b3
vicantlop <105176088+vicantlop@users.noreply.github.com>
2023-01-04 10:14:41 -0800

    created react app

---
+++ my-portfolio-page/src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Victor Lopez</h1>
+      <h3>Software Engineer, Full Stack Developer.</h3>
+    </div>
+  );
+}
+ 
+export default Home;
---
+++ my-portfolio-page/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Victor Lopez</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/work">Work</a>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
